3065b87fa1cf55a6bafbfb3e5971cffdd803dd54
Ousssama Driouech <thedriwsh@gmail.com>
2024-05-16 23:43:24 +0100

    create new directory to save scipt files

---
+++ assets/script/script.js
@@ -0,0 +1,16 @@
+var topics_Buttons = Array.from(document.getElementsByClassName('device-Card'));
+
+topics_Buttons.forEach(el => el.addEventListener("click", function () {
+    if (topics_Buttons.indexOf(el) == 0) {
+        
+    }
+    if (topics_Buttons.indexOf(el) == 1) {
+        
+    }
+    if (topics_Buttons.indexOf(el) == 2) {
+        
+    }
+    if (topics_Buttons.indexOf(el) == 3) {
+        
+    }
+}));
