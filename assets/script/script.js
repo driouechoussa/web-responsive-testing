c767d9d25686442adbbb554b7f04b2ac38086bc4
Ousssama Driouech <thedriwsh@gmail.com>
2024-05-17 23:49:47 +0100

    add effect when click on buttons

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,13 @@
-var topics_Buttons = Array.from(document.getElementsByClassName('device-Card'));
+const topics_Buttons = Array.from(document.getElementsByClassName('device-Card'));
+const Viewer_Device = document.getElementById('viewer-wrapp');
+
+
+
 
 topics_Buttons.forEach(el => el.addEventListener("click", function () {
+    topics_Buttons.forEach(btn => btn.classList.remove('topicFocusingLook'));
+    el.classList.add('topicFocusingLook');
+
     if (topics_Buttons.indexOf(el) == 0) {
         
     }
@@ -13,4 +20,14 @@ topics_Buttons.forEach(el => el.addEventListener("click", function () {
     if (topics_Buttons.indexOf(el) == 3) {
         
     }
+    
 }));
+
+
+
+
+
+function Devices_Dimensions() {
+    //head buttons 
+
+}
