6afa313f363115393757a414c2ddecfa28217794
Oussama Driouech <thedriwsh@gmail.com>
2024-05-22 22:19:53 +0100

    declair an object that will be dispplay an alert to user if he inputs an wrong value

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,8 @@
 const topics_Buttons = Array.from(document.getElementsByClassName('device-Card'));
 const Viewer_Device = document.getElementById('viewer-wrapp');
 
+const TopicHeader_items = document.querySelectorAll('Topic-item');
+
 topics_Buttons.forEach(el => el.addEventListener("click", function () {
     topics_Buttons.forEach(btn => btn.classList.remove('topicFocusingLook'));
     el.classList.add('topicFocusingLook');
@@ -17,7 +19,6 @@ topics_Buttons.forEach(el => el.addEventListener("click", function () {
     if (topics_Buttons.indexOf(el) == 3) {
         Devices_Dimensions(1920 , 1080);
     }
-    
 }));
 
 function Devices_Dimensions(width , height) {
@@ -26,20 +27,25 @@ function Devices_Dimensions(width , height) {
     Viewer_Device.style.height = height + 'px';
 }
 
-
 function takeTheLinkUrl() {
-
     var Viewer_Device_Value = String(document.getElementById('textField').value);
-        const urlPattern = new RegExp('^(https?:\\/\\/)?' + 
+        const urlPattern = new RegExp('^(https?:\\/\\/)?' +
             '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
-            '((\\d{1,3}\\.){3}\\d{1,3}))' + // 
+            '((\\d{1,3}\\.){3}\\d{1,3}))' + //
             '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
-            '(\\?[;&a-z\\d%_.~+=-]*)?' + 
-            '(\\#[-a-z\\d_]*)?$', 'i'); 
-    
+            '(\\?[;&a-z\\d%_.~+=-]*)?' +
+            '(\\#[-a-z\\d_]*)?$', 'i');
+
         if (urlPattern.test(Viewer_Device_Value)) {
            Viewer_Device.src = Viewer_Device_Value;
         } else {
-            alert('Please enter a valid URL.');
+           ShowMessageBox();
         }
+}
+
+function ShowMessageBox() {
+    const MessageBoxWrapper = document.createElement('div');
+    MessageBoxWrapper.setAttribute('id','MessageBox-Wrapper');
+    document.body.appendChild(MessageBoxWrapper);
+
 }
